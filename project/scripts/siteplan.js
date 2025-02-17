3556f58ab1f5658115f23aa4632ac8755695e3a4
ckangel <ckajeigbe1@gmail.com>
2025-02-17 19:06:49 +0000

    Final edits for siteplan

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -22,19 +22,19 @@ document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('email').value = '';
     });
     
-    // Footer Functionality
-    const yearElement = document.getElementById("year");
-    const currentYear = new Date().getFullYear();
-    if (yearElement) {
-      yearElement.textContent = currentYear;
-    } else {
-      console.log("Element with ID 'year' not found.");
-    }
-    const lastModifiedElement = document.getElementById("last-modified");
-    if (lastModifiedElement) {
-      const lastModifiedDate = document.lastModified;
-      lastModifiedElement.textContent = lastModifiedDate;
-    } else {
-      console.log("Element with ID 'last-modified' not found.");
-    }
-  });
+// Footer Functionality
+const yearElement = document.getElementById("year");
+const currentYear = new Date().getFullYear();
+if (yearElement) {
+  yearElement.textContent = currentYear;
+} else {
+  console.log("Element with ID 'year' not found.");
+}
+const lastModifiedElement = document.getElementById("last-modified");
+if (lastModifiedElement) {
+  const lastModifiedDate = document.lastModified;
+  lastModifiedElement.textContent = lastModifiedDate;
+} else {
+  console.log("Element with ID 'last-modified' not found.");
+}
+});
